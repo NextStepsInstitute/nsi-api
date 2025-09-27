bffe4d1159a747a14808a76f711b137d5692d48e
NextStepsInstitute <info@nextstepsinstitute.com>
2025-09-27 12:27:54 +0100

    Delete nsi-api directory

--- nsi-api/next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited directly by developers. It is used by
-// TypeScript to provide type definitions for Next.js specific globals.
